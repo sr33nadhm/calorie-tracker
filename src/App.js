4fbce24098c49c0c3dfb241ed650433008e0ff4e
Sreenadh M <sreenathpurushothaman@gmail.com>
2022-11-16 18:39:20 +0530

    Added hash router for gh pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Dashboard from "pages/Dashboard";
 import Home from "pages/Home";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
@@ -12,18 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/">
-          <Route
-            exact={true}
-            strict
-            index
-            element={<Home setUser={setUser} />}
-          />
-          <Route
-            exact={true}
-            strict
-            path="dashboard"
-            element={<Dashboard user={user} />}
-          />
+          <Route exact={true} strict index element={<Home setUser={setUser} />} />
+          <Route exact={true} strict path="dashboard" element={<Dashboard user={user} />} />
         </Route>
       </Routes>
     </Router>
